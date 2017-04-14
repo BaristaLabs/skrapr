bc3236b2f828982fc28c4a8cc41ac1ff3d296f3d
Sean McLellan <sean.mclellan@sixconcepts.com>
2017-04-14 16:01:25 -0400

    Add HighlightRectTask; Fix HighlightDomElementTask

--- src/BaristaLabs.Skrapr.Core/Tasks/HighlightDomElementTask.cs
+++ src/BaristaLabs.Skrapr.Core/Tasks/HighlightDomElementTask.cs
@@ -24,68 +24,14 @@
 
         public async Task PerformTask(SkraprContext context)
         {
-            System.Threading.Thread.Sleep(2000);
             var layoutTreeNode = await context.DevTools.GetLayoutTreeNodeForDomElement(Selector);
             if (layoutTreeNode == null)
                 return;
 
-            await context.Session.SendCommand(new Dom.SetInspectModeCommand
-            {
-                Mode = Dom.InspectMode.SearchForNode,
-                HighlightConfig = new Dom.HighlightConfig
-                {
-                    ContentColor = new Dom.RGBA
-                    {
-                        R = 0,
-                        G = 0,
-                        B = 255,
-                        A = 0.7
-                    }
-                }
-            });
-
-            await context.Session.SendCommand(new Dom.HighlightRectCommand
-            {
-                X = 100,
-                Y = 100,
-                Height = 100,
-                Width = 100,
-                Color = new Dom.RGBA
-                {
-                    R = 1,
-                    G = 1,
-                    B = 255,
-                    A = 0.5
-                },
-                OutlineColor = new Dom.RGBA
-                {
-                    R = 255,
-                    G = 1,
-                    B = 1,
-                    A = 1
-                }
-            });
-
-            await context.Session.SendCommand(new Dom.HideHighlightCommand());
-            await context.Session.SendCommand(new Dom.SetInspectedNodeCommand
-            {
-                NodeId = layoutTreeNode.NodeId
-            });
-
             var response = await context.Session.SendCommand<Dom.HighlightNodeCommand, Dom.HighlightNodeCommandResponse>(new Dom.HighlightNodeCommand
             {
                 NodeId = layoutTreeNode.NodeId,
-                HighlightConfig = new Dom.HighlightConfig
-                {
-                    ContentColor = new Dom.RGBA
-                    {
-                        R = 0,
-                        G = 0,
-                        B = 255,
-                        A = 0.7
-                    }
-                }
-                //HighlightConfig = HighlightConfig
+                HighlightConfig = HighlightConfig
             });
         }
     }
---
+++ src/BaristaLabs.Skrapr.Core/Tasks/HighlightRectTask.cs
@@ -0,0 +1,92 @@
+﻿namespace BaristaLabs.Skrapr.Tasks
+{
+    using Newtonsoft.Json;
+    using System.Threading.Tasks;
+    using Dom = ChromeDevTools.DOM;
+
+    public class HighlightRectTask : ITask
+    {
+        public string Name
+        {
+            get { return "HighlightRect"; }
+        }
+
+        /// <summary>
+        /// X coordinate
+        /// </summary>
+        [JsonProperty("x")]
+        public long X
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Y coordinate
+        /// </summary>
+        [JsonProperty("y")]
+        public long Y
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Rectangle width
+        /// </summary>
+        [JsonProperty("width")]
+        public long Width
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Rectangle height
+        /// </summary>
+        [JsonProperty("height")]
+        public long Height
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// The highlight fill color (default: transparent).
+        /// </summary>
+        [JsonProperty("color", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Dom.RGBA Color
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// The highlight outline color (default: transparent).
+        /// </summary>
+        [JsonProperty("outlineColor", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public Dom.RGBA OutlineColor
+        {
+            get;
+            set;
+        }
+
+        public async Task PerformTask(SkraprContext context)
+        {
+            await context.Session.SendCommand(new Dom.HighlightRectCommand
+            {
+                X = X,
+                Y = Y,
+                Height = Height,
+                Width = Width,
+                Color = Color,
+                OutlineColor = OutlineColor
+            });
+        }
+    }
+}
