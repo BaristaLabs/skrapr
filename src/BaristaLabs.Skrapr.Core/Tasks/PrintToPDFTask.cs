60817e12259a5c82fe53f0700be944e9a9027e80
Sean McLellan <sean.mclellan@sixconcepts.com>
2017-04-13 21:51:27 -0400

    Add PrintToPDF task (tip-of-tree)

---
+++ src/BaristaLabs.Skrapr.Core/Tasks/PrintToPDFTask.cs
@@ -0,0 +1,20 @@
+﻿namespace BaristaLabs.Skrapr.Tasks
+{
+    using Newtonsoft.Json.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class PrintToPDFTask : ITask
+    {
+        public string Name
+        {
+            get { return "PrintToPDF"; }
+        }
+
+        public async Task PerformTask(SkraprContext context)
+        {
+            var base64Pdf = await context.Session.SendCommand("Page.printToPDF", JToken.FromObject(new object()), CancellationToken.None);
+
+        }
+    }
+}
