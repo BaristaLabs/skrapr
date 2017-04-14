4f0935bfa239c4fb7545232e678677a789c31e49
Sean McLellan <sean.mclellan@sixconcepts.com>
2017-04-13 22:00:37 -0400

    Add set attribute value task

---
+++ src/BaristaLabs.Skrapr.Core/Tasks/SetAttributeValueTask.cs
@@ -0,0 +1,45 @@
+﻿namespace BaristaLabs.Skrapr.Tasks
+{
+    using System.Threading.Tasks;
+    using Dom = ChromeDevTools.DOM;
+
+    public class SetAttributeValueTask : ITask
+    {
+        public string Name
+        {
+            get { return "SetAttributeValue"; }
+        }
+
+        public string Selector
+        {
+            get;
+            set;
+        }
+
+        public string AttributeName
+        {
+            get;
+            set;
+        }
+
+        public string AttributeValue
+        {
+            get;
+            set;
+        }
+
+        public async Task PerformTask(SkraprContext context)
+        {
+            var nodeId = await context.DevTools.GetNodeForSelector(Selector);
+            if (nodeId < 1)
+                return;
+
+            await context.Session.SendCommand(new Dom.SetAttributeValueCommand
+            {
+                NodeId = nodeId,
+                Name = AttributeName,
+                Value = AttributeValue
+            });
+        }
+    }
+}
