c36a070f25b1f9d376c592800d08446c06321a74
Sean McLellan <sean.mclellan@sixconcepts.com>
2017-04-16 13:52:11 -0400

    Implement basic screenshot task.

--- src/BaristaLabs.Skrapr.Core/Tasks/ScreenshotTask.cs
+++ src/BaristaLabs.Skrapr.Core/Tasks/ScreenshotTask.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BaristaLabs.Skrapr.Tasks
+﻿namespace BaristaLabs.Skrapr.Tasks
 {
-    class ScreenshotTask
+    using Page = ChromeDevTools.Page;
+    using System.Threading.Tasks;
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// Represents a task that takes a screenshot of a page.
+    /// </summary>
+    public class ScreenshotTask : SkraprTask
     {
+        public override string Name
+        {
+            get { return "Screenshot"; }
+        }
+
+        public string OutputFilename
+        {
+            get;
+            set;
+        }
+
+        public ScreenshotTask()
+        {
+            OutputFilename = "screenshot.png";
+        }
+
+        public override async Task PerformTask(ISkraprWorker worker)
+        {
+            var result = await worker.Session.Page.CaptureScreenshot(new Page.CaptureScreenshotCommand());
+            var imageBytes = Convert.FromBase64String(result.Data);
+            File.WriteAllBytes(OutputFilename, imageBytes);
+        }
     }
 }
